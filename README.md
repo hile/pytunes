462bbb8362944ce7f2cbe06771e8fe50b04cbeeb
Ilkka Tuohela <ilkka.tuohela@codento.com>
2017-07-14 21:31:57 +0300

    Move itunesd code to pytunes.daemon

--- README.md
+++ README.md
@@ -10,3 +10,36 @@ itunes has it's own notifications.
 
 This module uses apple script bridge and only works on OS X. No windows support is planned.
 
+iTunes index database
+---------------------
+
+There is a custom sqlite database for itunes track indexes. This database is used when
+playing tracks by path from shell, like:
+
+```
+itunes play 'Music/Library/ACDC/Back in Black/01 Hells Bells.m4a'
+```
+
+The command works without the database, but can be very slow, because there is no direct
+way to lookup track ID from itunes and itunes.play(path) actually imports track, not just
+plays it.
+
+Creating index of itunes tracks to sqlite is thus recommended and can be done with:
+
+```
+itunes update-index
+```
+
+iTunes status daemon
+--------------------
+
+The itunesd daemon currently does very little. Currently it's only purpose is to log any
+played tracks with timestamp to ~/Library/Logs/itunes.log file.
+
+You can extend the pytunes.daemon.iTunesDaemon class process_track_change method to do
+something else. This method is called whenever itunes track changes or playback status
+is changed, and receives playback status and track details as arguments.
+
+There is example example launch agent file in examples/com.tuohela.net.itunesd.plist
+that can be used to load itunesd with launchctl. This example expects you use homebrew's
+python2.7 from /usr/local/bin.
---
+++ pytunes/daemon.py
@@ -0,0 +1,58 @@
+"""
+iTunes background daemon
+"""
+
+import os
+
+from pytunes import iTunesError
+from pytunes.status import iTunesStatus
+
+class iTunesDaemon(object):
+    """Background daemon for itunes status
+
+    Daemon to monitor itunes status changes. By default just logs tracks played.
+    """
+    def __init__(self, logfile=None):
+        self.monitor = iTunesStatus()
+        self.logfile = logfile
+
+    def run(self):
+        """Run daemon
+
+        """
+        while True:
+            status, details = self.monitor.next()
+            try:
+                self.process_track_change(status, details)
+            except iTunesError as e:
+                print e
+
+    def log_track_change(self, details):
+        """Log track changes
+
+        Logs timestamp and path of track played to logfile, if defined.
+        """
+        if self.logfile is None:
+            return
+
+        path =  os.path.expandvars(os.path.expanduser(self.logfile))
+        try:
+            with open(path, 'w') as f:
+                f.write('{0} {1}\n'.format(
+                    details['started'],
+                    details['path'],
+                ))
+                f.flush()
+        except OSError as e:
+            raise iTunesError('Error writing to {0}: {1}'.format(args.log_file, e))
+        except IOError as e:
+            raise iTunesError('Error writing to {0}: {1}'.format(args.log_file, e))
+
+    def process_track_change(self, status, details):
+        """Process track changes
+
+        Receives current status and track details as arguments. By default logs
+        track changes, override to do something else.
+        """
+        if status == 'playing' and details:
+            self.log_track_change(details)
